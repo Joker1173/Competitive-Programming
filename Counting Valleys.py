079692ca41a1c9e7fb2b02817d726bdd6809a528
Joker1173 <97368501+Joker1173@users.noreply.github.com>
2023-01-22 15:07:55 +0300

    Create Counting Valleys.py
    HackerRank

---
+++ Counting Valleys.py
@@ -0,0 +1,12 @@
+def countingValleys(steps, path):
+    # Write your code here
+    valleys=0
+    tmp=0
+    for i in range(steps):
+        if path[i] =="D":
+            tmp-=1
+        else:
+            tmp+=1
+        if tmp==-1 and path[i+1]=="U":
+            valleys+=1
+    return valleys
