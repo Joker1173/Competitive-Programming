ff5fdcde8628945e7b219f62101de18745efa60e
Joker1173 <97368501+Joker1173@users.noreply.github.com>
2023-02-02 11:39:51 +0300

    Update Array_With_Elements_Not_Equal_to_Average_of_Neighbors.py

--- Array_With_Elements_Not_Equal_to_Average_of_Neighbors.py
+++ Array_With_Elements_Not_Equal_to_Average_of_Neighbors.py
@@ -1,14 +1,5 @@
 class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
-        i=1
-        while i < len(nums)-1:
-            if (nums[i-1] + nums[i+1]) / 2 == nums[i]:        #compare nums[i] to the average of it's previous and it's next elements
-                tmp=nums[i]                                         #swap nums[i] with the element next to it 
-                nums[i]=nums[i+1]
-                nums[i+1]=tmp
-            if (nums[i-2] + nums[i]) / 2 == nums[i-1]:        #check for error caused by current swapping on previous elements
-                i-=1
-            else:
-                i+=1
-        return nums
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums=sorted(map(int,nums))
+        return str(nums[len(nums)-k])n nums
                 
